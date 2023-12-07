d354ed4c512dbfe75a691fab4765e1283dc7a0ee
Matteo Affatato <matteoaffatato@MacBook-Air-di-Matteo.local>
2023-12-07 17:20:11 +0100

    esercizi_condizioni_cicli

---
+++ script.js
@@ -0,0 +1,94 @@
+// //  ESERCIZIO 1
+
+//   let v = 29;
+//   if (v<18) {
+//       console.log("insufficiente");
+//   }
+//   else if (v>=18 && v<21) {
+//       console.log("Sufficiente");
+//   }
+//   else if (v>=18 && v<24) {
+//       console.log("buono");
+//   }
+//   else if (v>=18 && v<27) {
+//       console.log("distinto");
+//   }
+//   else if (v>=18 && v<=29) {
+//       console.log("ottimo");
+//   }
+//   else if (v=30) {
+//       console.log("eccellente");
+//   }
+ 
+
+// let v = 29;
+// switch (true) {
+//     case v<18 :
+//         console.log("insufficiente")
+//     break;
+
+//     case v>=18 && v<21 :
+//         console.log("Sufficiente")
+//     break;
+
+//     case v>=18 && v<24 :
+//         console.log("buono")
+//     break;
+//     case (v>=18 && v<27) :
+//         console.log("distinto")
+//     break;
+
+//     case v>=18 && v<=29 :
+//         console.log("ottimo")
+//     break;
+
+//     case v=30 :
+//         console.log ("eccellente")
+//     break;
+    
+// }
+
+
+// ESERCIZIO 2
+
+//  let t = prompt ("inserisci temperatura")
+
+//  if (t<20 && t<=0) {
+//      console.log ("Non ci sono più mezze stagioni");
+    
+//  } else if (t>=30) {
+//      console.log ("lu mare, lu sole, lu ientu");
+//  }
+//    else if (t< 30 && t> 20) {
+//      console.log("mi dia una peroni sudata");
+//  } else if (t< 0 && t>= -10) {
+//      console.log("non e’ tanto il freddo quanto l’umidità");
+//  }
+//  else if (t< -10) {
+//      console.log("copriti… ancora ti raffreddi");
+//  }
+
+
+
+
+// Esercizio 3
+
+// let drink = prompt("Inserisci la tua bevanda");
+// if (drink == 1) {
+//     console.log('E’ stata selezionata l’acqua')
+// } else if (drink == 2) {
+//     console.log ('E’ stata selezionata coca cola')
+// } else if (drink == 3) {
+//     console.log('E’stata selezionata la birra')
+   
+// } else {
+//     alert ('seleziona la tua bevanda!')
+// }
+
+
+
+
+
+
+   
+
